dd38a9b566221a3ff1afcb00310bc5eb308227e7
Patrick Philippy <pmp2516@rit.edu>
2024-12-16 16:27:52 -0500

    visualization code

--- project_4.py
+++ project_4.py
@@ -9,10 +9,15 @@ from regression import linear_least_squares
 from inference import linear_inference, softmax
 import cv2 as cv
 
+HEIGHT = 112
+WIDTH = 92
+
 def main():
     images, labels = read_dataset('./image_data.npy', './user_ids.npy')
     pcs = get_pcs(images)
-    visualize_reconstructions(images, pcs)
+    accuracies = eval_pcs(images, pcs, labels, k_vals = np.linspace(10, 1000, num=100, dtype=int))
+    plt.plot(accuracies)
+    plt.show()
 
 def save_pcs(images, path):
     p, cov = pca(images)
@@ -26,20 +31,21 @@ def get_pcs(images, path='./pcs.npy', force=False):
         pcs = np.load(path)
     return pcs
 
-def eval_pcs(images, pcs, labels):
+def eval_pcs(images, pcs, labels, k_vals = None):
+    if k_vals is None:
+        k_vals = np.linspace(10, HEIGHT * WIDTH, num=1000, dtype=int)
     accuracies = []
-    for k in range(10, 1000, 10):
-        k = 1
+    print('k,accuracy')
+    for k in k_vals:
         p = pcs[:, :k]
         images_pcs = images @ p # To PC basis
         w = linear_least_squares(images_pcs, labels)
         if w is None:
             continue
-        print(f'w: {w.shape}, p: {p.shape}, images_pcs: {images_pcs.shape}, images: {images.shape}')
         output = linear_inference(images_pcs, w)
         predicted = np.argmax(output, axis=1)
         accuracy = np.mean(predicted == labels)
-        print(f'k: {k:50} accuracy: {accuracy:10}')
+        print(f'{k},{accuracy}')
         accuracies.append(accuracy)
     return accuracies
         
@@ -49,13 +55,12 @@ def project_and_reconstruct(images, pcs, k):
     images_reconstructed = images_pcs @ p.T
     return images_reconstructed
 
-def visualize_reconstructions(images, pcs):
-    num_reconstructions = 25
-    height, width = 112, 92
-    images_reconstructed_all = np.zeros((400, num_reconstructions + 1, height * width))
+def visualize_reconstructions(images, pcs, k_vals = None):
+    if k_vals is None:
+        k_vals = np.linspace(1, HEIGHT * WIDTH, num=25, dtype=int)
+    num_reconstructions = len(k_vals)
+    images_reconstructed_all = np.zeros((400, num_reconstructions + 1, HEIGHT * WIDTH))
     images_reconstructed_all[:, 0] = images
-    # k_vals = np.linspace(10304, 0, num=num_reconstructions, dtype=int)
-    k_vals = np.logspace(0, np.log10(height * width), num=num_reconstructions, dtype=int)
     for i, k in enumerate(k_vals):
         images_reconstructed_all[:, i+1] = project_and_reconstruct(images, pcs, k)
 
@@ -64,7 +69,7 @@ def visualize_reconstructions(images, pcs):
     for image_and_reconstructions in images_reconstructed_all:
         fig, axes = plt.subplots(1, num_reconstructions+1, figsize=(20,5))
         for j, ax in enumerate(axes):
-            ax.imshow(image_and_reconstructions[j].reshape(height, width), cmap='gray')
+            ax.imshow(image_and_reconstructions[j].reshape(HEIGHT, WIDTH), cmap='gray')
             ax.set_title('original' if j == 0 else f"k={k_vals[j-1]}")
             ax.axis('off')
         plt.tight_layout()
